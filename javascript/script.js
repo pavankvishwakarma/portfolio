2357215dfb817af971095df8b526f99ba64d173b
pavankvishwakarma <pavankvishwakarma@gmail.com>
2024-05-27 13:38:40 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,8 @@ function updateClock() {
   var minutes = now.getMinutes();
   var seconds = now.getSeconds();
   var meridiem = hours >= 12 ? 'PM' : 'AM'; // Determine AM or PM
-
+  var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
   // Convert hours to 12-hour format
   hours = hours % 12;
   hours = hours ? hours : 12; // "0" should be "12" in 12-hour format
@@ -16,14 +17,14 @@ function updateClock() {
   var timeString = 'Time : ' + hours + ':' + minutes + ':' + seconds + ' ' + meridiem;
   document.getElementById('clock').textContent = timeString;
 
-  var month = now.getMonth() + 1;
+  var month = now.getMonth() ;
   var day = now.getDate();
   var year = now.getFullYear();
 
-  month = month < 10 ? '0' + month : month;
+
   day = day < 10 ? '0' + day : day;
 
-  var dateString ='Date : '+ day + '/' + month + '/' + year;
+  var dateString ='Date : '+ monthNames[month] + " " + day + ', ' + year;
   document.getElementById('date').textContent = dateString;
 }
 
